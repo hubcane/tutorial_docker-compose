3c61ae55090a0c863c0ad7ca11c1558519222dfd
hubcane <hubcane@gmail.com>
2022-09-05 21:44:00 +0900

    2nd commit

--- cjproejct/tutorial_django_1/fc_community/fc_community/settings.py
+++ cjproejct/tutorial_django_1/fc_community/fc_community/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6q82t9ixp*0b6)hy7#b5uwl$lf_yd9w9133wtyo3odhiz027q7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,7 +118,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
